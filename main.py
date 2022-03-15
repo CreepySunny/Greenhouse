a71cf5e17306b1a2ff88c333061183ed4866aabb
Sunny <40369915+CreepySunny@users.noreply.github.com>
2022-03-15 11:27:20 +0100

    Passes randomly generated intergors to the webpage.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+from random import randint
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html.jinja', t_sens=randint(20, 36), h_sens=randint(40, 80))
 
 if __name__ == "__main__":
     app.run(debug=True)
