5769594950b202576528720649ea5ca8ce6456a4
Sunny <40369915+CreepySunny@users.noreply.github.com>
2022-03-15 13:28:25 +0100

    Remame main.py file and re-organized the imports.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, url_for
 from random import randint
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """The homepage funciton"""
     return render_template('index.html.jinja', t_sens=randint(20, 36), h_sens=randint(40, 80))
 
 if __name__ == "__main__":
