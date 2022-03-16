13a7d72974e063ac8549a62c795181d356d2035d
Sunny <40369915+CreepySunny@users.noreply.github.com>
2022-03-16 14:48:15 +0100

    Added time and humidity. Passing the data with dictionay.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from random import randint
-from flask import Flask, render_template, url_for
+from time import strptime
+from flask import Flask, render_template
+import datetime
 
+data = {}
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     """The homepage funciton"""
-    return render_template('index.html.jinja', t_sens=randint(20, 36), h_sens=randint(40, 80))
+    data["temp"] = randint(15, 40)
+    data["humi"] = randint(0, 100)
+    data["light"] = randint(0, 1000)
+    data["dt"] = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    return render_template('index.html.jinja', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
