3975ce8d73599d03964306add391340adf543228
Sunny <40369915+CreepySunny@users.noreply.github.com>
2022-03-16 15:02:04 +0100

    Generation of random data moved to its own function.

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from random import randint
-from time import strptime
-from flask import Flask, render_template
 import datetime
+from flask import Flask, render_template
+
 
 data = {}
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    """The homepage funciton"""
+def gen_rand_data():
+    """Generates random data for debug purposes"""
     data["temp"] = randint(15, 40)
     data["humi"] = randint(0, 100)
     data["light"] = randint(0, 1000)
     data["dt"] = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+@app.route("/")
+def index():
+    """The homepage funciton"""
+    gen_rand_data()
     return render_template('index.html.jinja', data=data)
 
 if __name__ == "__main__":
