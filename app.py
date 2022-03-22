018c4b005f92b59a9f38a428296e0f9223c16d77
Sunny <40369915+CreepySunny@users.noreply.github.com>
2022-03-22 09:29:56 +0100

    Changed the datetime improt to more efficient. Created function to get the vurrent time and date.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from random import randint
-import datetime
+from datetime import datetime
 from flask import Flask, render_template
+import CustomPymata4
 
 
 data = {}
 app = Flask(__name__)
+
+def get_date_time():
+    """Gets the current time and date."""
+    data["dt"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 def gen_rand_data():
     """Generates random data for debug purposes"""
     data["temp"] = randint(15, 40)
     data["humi"] = randint(0, 100)
     data["light"] = randint(0, 1000)
-    data["dt"] = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 @app.route("/")
 def index():
